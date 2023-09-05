0d3f8b92602aae3965fe8e38bd6b57c6af55d983
misikira <misikirteka@gmail.com>
2023-09-05 13:54:40 +0300

    create a movie list component

--- idrama/src/App.js
+++ idrama/src/App.js
@@ -54,6 +54,7 @@ const average = (arr) =>
       <>
        <NavBar />
        <Main />
+       <ListBox />
       </>
     );
   }
@@ -99,9 +100,9 @@ function Search(){
 }
 
   function Main (){
-  const [movies, setMovies] = useState(tempMovieData);
+  
   const [watched, setWatched] = useState(tempWatchedData);
-  const [isOpen1, setIsOpen1] = useState(true);
+  
   const [isOpen2, setIsOpen2] = useState(true);
 
   const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
@@ -109,31 +110,7 @@ function Search(){
   const avgRuntime = average(watched.map((movie) => movie.runtime));
       return (
         <main className="main">
-        <div className="box">
-          <button
-            className="btn-toggle"
-            onClick={() => setIsOpen1((open) => !open)}
-          >
-            {isOpen1 ? "–" : "+"}
-          </button>
-          {isOpen1 && (
-            <ul className="list">
-              {movies?.map((movie) => (
-                <li key={movie.imdbID}>
-                  <img src={movie.Poster} alt={`${movie.Title} poster`} />
-                  <h3>{movie.Title}</h3>
-                  <div>
-                    <p>
-                      <span>🗓</span>
-                      <span>{movie.Year}</span>
-                    </p>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-
+        
         <div className="box">
           <button
             className="btn-toggle"
@@ -195,3 +172,34 @@ function Search(){
 
     }
 
+function ListBox (){
+  const [isOpen1, setIsOpen1] = useState(true);
+  const [movies, setMovies] = useState(tempMovieData);
+  return(
+    <div className="box">
+          <button
+            className="btn-toggle"
+            onClick={() => setIsOpen1((open) => !open)}
+          >
+            {isOpen1 ? "–" : "+"}
+          </button>
+          {isOpen1 && (
+            <ul className="list">
+              {movies?.map((movie) => (
+                <li key={movie.imdbID}>
+                  <img src={movie.Poster} alt={`${movie.Title} poster`} />
+                  <h3>{movie.Title}</h3>
+                  <div>
+                    <p>
+                      <span>🗓</span>
+                      <span>{movie.Year}</span>
+                    </p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+
+  )
+}
