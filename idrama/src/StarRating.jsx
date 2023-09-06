d4291893cb236fc4a88d83e5c9942e17a791e4fd
misikira <misikirteka@gmail.com>
2023-09-06 11:44:23 +0300

    add full star rate for rating and empty for rest

--- idrama/src/StarRating.jsx
+++ idrama/src/StarRating.jsx
@@ -13,12 +13,12 @@ const textstyle={
     margin:"0"
 }
 export default function StarRating({maxrate =5}) {
-    const [rating,setrating]=useState(1)
+    const [rating,setrating]=useState(0)
   return (
     <div style={contaierstyle}>
         <div style={starscontainer}>
             {Array.from({length:maxrate},(_,i)=>(
-<Star i={1} onRate={()=>handlerate(1 +i)}/>))}
+<Star i={1} onRate={()=>handlerate(1 +i)} full={rating >= i +1}/>))}
         </div>
         <p style={textstyle}>{rating || ""}</p>
     </div>
@@ -34,9 +34,9 @@ const starstyle={
     display:"block",
     cusrsor:"pointer"
 }
-function Star({onRate}){
-    return(
-    <span role="button" style={starstyle} onClick={onRate}>
+function Star({onRate,full}){
+    return (
+    <span role="button" style={starstyle} onClick={onRate}>{ full?
 
         <svg
   xmlns="http://www.w3.org/2000/svg"
@@ -47,7 +47,18 @@ function Star({onRate}){
   <path
     d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
     />
-</svg>
-    </span>    
-    )
+</svg>:<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke="#000"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="{2}"
+    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+  /></svg>}
+    </span>
+    );
 }
