aa3a87737d78489a3be523b1a7fa64df3d7ad59a
misikira <misikirteka@gmail.com>
2023-09-06 14:32:45 +0300

    make the star rate more reusale

--- idrama/src/StarRating.jsx
+++ idrama/src/StarRating.jsx
@@ -7,13 +7,23 @@ const contaierstyle = {
 const starscontainer = {
 	display: "flex",
 };
-const textstyle = {
-	lineHeight: "1",
-	margin: "0",
-};
-export default function StarRating({ maxrate = 5 }) {
+
+export default function StarRating({
+	maxrate = 5,
+	color = "#fcc419",
+	size = 48,
+}) {
 	const [rating, setrating] = useState(0);
 	const [tempRating, settempRating] = useState(0);
+	function handlerate(rating) {
+		setrating(rating);
+	}
+	const textstyle = {
+		lineHeight: "1",
+		margin: "0",
+		color,
+		fontSize: `${size / 1.5}px`,
+	};
 	return (
 		<div style={contaierstyle}>
 			<div style={starscontainer}>
@@ -24,24 +34,24 @@ export default function StarRating({ maxrate = 5 }) {
 						full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
 						onhoverin={() => settempRating(i + 1)}
 						onhoverout={() => settempRating(0)}
+						color={color}
+						size={size}
 					/>
 				))}
 			</div>
 			<p style={textstyle}>{tempRating || rating || ""}</p>
 		</div>
 	);
-	function handlerate(rating) {
-		setrating(rating);
-	}
 }
 
-const starstyle = {
-	width: "48px",
-	height: "48px",
-	display: "block",
-	cusrsor: "pointer",
-};
-function Star({ onRate, full, onhoverin, onhoverout }) {
+function Star({ onRate, full, onhoverin, onhoverout, color, size }) {
+	const starstyle = {
+		width: `${size}px`,
+		height: `${size}px`,
+		display: "block",
+		cusrsor: "pointer",
+		color,
+	};
 	return (
 		<span
 			role="button"
@@ -54,8 +64,8 @@ function Star({ onRate, full, onhoverin, onhoverout }) {
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					viewBox="0 0 20 20"
-					fill="#000"
-					stroke="#000"
+					fill={color}
+					stroke={color}
 				>
 					<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
 				</svg>
@@ -64,7 +74,7 @@ function Star({ onRate, full, onhoverin, onhoverout }) {
 					xmlns="http://www.w3.org/2000/svg"
 					fill="none"
 					viewBox="0 0 24 24"
-					stroke="#000"
+					stroke={color}
 				>
 					<path
 						strokeLinecap="round"
--- idrama/src/index.js
+++ idrama/src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // import './index.css';
 // import App from './App';
-import reportWebVitals from './reportWebVitals';
-import StarRating from './StarRating';
+import reportWebVitals from "./reportWebVitals";
+import StarRating from "./StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxrate={5}/>
-  </React.StrictMode>
+	<React.StrictMode>
+		{/* <App /> */}
+		<StarRating maxrate={5} />
+		<StarRating size={24} />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
